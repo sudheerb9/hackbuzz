1b08b565804ab0fec792653737f4bef3ec972ea7
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-02-28 19:27:58 +0530

    ok

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser= require('body-parser');
 var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
