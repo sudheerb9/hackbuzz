01ab990818072d1da3d2699d48efcbd21d30a931
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-02-28 19:35:30 +0530

    ok

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({ secret: 'keyboard cat', key: 'sid'}));
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', indexRouter);
+app.use(indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
